155242863b02f862f6d38f06c4ef82c76b5955f5
arcticsnow <sfilhol@protonmail.com>
2024-04-01 18:47:23 +0200

    added ploting function for weather

--- snowpyt/weather.py
+++ snowpyt/weather.py
@@ -12,23 +12,79 @@ See Github issue: https://github.com/ArcticSnow/snowpyt/issues/6
 """
 
 
-
 import pandas as pd 
 import matplotlib.pyplot as plt  
 import numpy as np  
 
 
 
-def plot_weather_history(df):
+def plot_weather_history(df, resampling='12H', var_to_plot=['t', 'ws', 'sd']):
+    """ Plot weather history in light of snow metamorphism history
+    
+    Args:
+        df (dataframe): Dataframe with timeseries 
+                            temperature (degC): 't'
+                            wind speed (m/s): 'ws'
+                            snowdepth (cm): 'sd' 
+        resampling (str, optional): Resampling to apply to the timeseries, assuming hourly input
+        var_to_plot (list, optional): List of variable to plot in a given order.
+    """
+    # resample timseries to coarser time resolution
+    if resampling is not None:
+        df_res = df.resample(resampling).mean()
+    else:
+        df_res = df
+    
+    def ax_plot_temp(df_sub, ax):
+        df_sub['t_below_freezing'] = df_sub.t * (df_sub.t <=0)
 
-	fig, ax = plt.subplots(3,1,sharex=True)
+        df_sub.t.plot(ax=ax)
+        ax.fill_between(df_sub.index, df_sub.t_below_freezing, df_sub.t, color='r')
+        ax.set_ylabel('Temperature\n[$^{o}C$]')
+        
+        return ax
+        
+        
+    def ax_plot_ws(df_sub, ax):
+        
+        # curve used for shading
+        df_sub['t_below_freezing'] = df_sub.t * (df_sub.t <=0)
+        df_sub['ws_above_drifting'] = df_sub.ws * ((df_sub.ws >=6)) 
+        df_sub['ws_below_drifting'] = 6
+        df_sub.ws_below_drifting.loc[df_sub.ws<=6] = df_sub.ws.loc[df_sub.ws<=6]
+        
+        df_sub.ws.plot(ax=ax)
+        #df_sub.ws_below_drifting.plot(ax=ax[1])
+        ax.fill_between(df_sub.index, df_sub.ws_below_drifting, df_sub.ws, color='g')
+        ax.set_ylabel('Wind speed\n[$m.s^{-1}$]')
+        
+        return ax
+    
+    def ax_plot_sd(df_sub, ax):
+        df_sub.sd.plot(ax=ax, label=None)
+        ax.fill_between(df_sub.index, 0, df_sub.sd.max()*1.2, where=df_sub.ws>=6, color='g', alpha=0.2, edgecolor=None, label='$P_{drifting}>0$')
+        ax.fill_between(df_sub.index, 0, df_sub.sd.max()*1.2, where=df_sub.t>=0, color='r', alpha=0.2, edgecolor=None, label='melt')
+        ax.legend()
+        ax.set_ylabel('Snow depth\n[cm]')
+        
+        return ax
+        
 
-	# ax[0] plot temperature
+    fig, ax = plt.subplots(len(var_to_plot),1, sharex=True, figsize=(12,8))
+    
+    for i, var in enumerate(var_to_plot):
+        if var == 't':
+            ax[i] = ax_plot_temp(df_res, ax[i])
+        elif var == 'ws':
+            ax[i] = ax_plot_ws(df_res, ax[i])
+        elif var == 'sd':
+            ax[i] = ax_plot_sd(df_res, ax[i])
+        elif var not in ['t', 'ws', 'sd']:
+            raise Error(f'--> {var} is not available for plotting')
+            
+    plt.tight_layout()
 
 
-	# ax[1] plot wind
 
-
-	# ax[2] plot precipitation
 
 
